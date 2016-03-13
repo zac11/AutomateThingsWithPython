2b79ed2adebd1409613e2f06f4d4ba9bba417cee
zac11 <ydv.rahul09@gmail.com>
2016-03-13 10:52:22 +0530

    List basics

---
+++ list_value_change.py
@@ -0,0 +1,15 @@
+list1=['messi','ronaldo','costa','ibra','rooney']
+
+print(list1)
+#replacing rooney with hazard
+list1[4]='hazard'
+
+#print new list
+print(list1)
+
+#replacing costa with hazard again
+list1[2]=list1[4]
+
+print(list1)
+
+
