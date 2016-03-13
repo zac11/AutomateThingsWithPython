b6b87084912579c678b6ab358cb0b0e66280f0d6
zac11 <ydv.rahul09@gmail.com>
2016-03-13 10:32:29 +0530

    List basics

---
+++ list_basic.py
@@ -0,0 +1,18 @@
+#simple list
+spam=['messi','ronaldo','costa','ibra']
+print(spam[1:4])
+print(spam[:4])
+print(spam[:-1])
+print(spam[-1:])
+print(spam[3:])
+
+#list within a list
+spam2=[['cat','bat'],['hat','sat']]
+print(spam2[1][1])
+print(spam2[-1][0])
+print(spam2[0][-1])
+print(spam2[-1][-1])
+
+#length of a list
+print(len(spam2))
+print(len(spam))
