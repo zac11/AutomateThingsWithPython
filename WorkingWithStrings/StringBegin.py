45a2c145d4169d33b6444dea2a865d58c1c59dc1
zac11 <ydv.rahul09@gmail.com>
2016-06-23 10:37:46 +0530

    Added Strings

--- WorkingWithStrings/StringBegin.py
+++ WorkingWithStrings/StringBegin.py
@@ -3,3 +3,37 @@ This is a python string which recognises a literal
 """
 string1="This is Alice's cat"
 print(string1)
+
+"""
+This will recognise a escape character
+"""
+string2='This is what Prachi\'s usual conversation is'
+print(string2)
+
+"""
+This will also recognize the escape character newline
+"""
+
+string3 = "Hey\n Wassup \n Nothing \n Hmmmm \n k \n Job Job Job Job "
+print(string3)
+
+
+'''
+This is a raw string '''
+print(r'This is the raw string that I have initialized for that shit\'s lingo')
+
+"""
+This is a multi line string that I have used
+"""
+
+print(""" My Reply to all above is that
+
+You are one little stinky bitch is that what you are idiot
+But you, little one, is being missed
+And my cat's being arrested for burglary, arson, treason is not something I have made
+in my mind
+I am going bonkers
+
+Sincerely
+Zac
+""")
