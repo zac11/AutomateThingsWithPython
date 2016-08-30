08cdc713854a20173e6e4fba40ba9c663a1ed209
zac11 <rahul@9988>
2016-08-30 10:10:23 +0530

    Added new dictionary code

---
+++ Dictionary/SetDefault.py
@@ -0,0 +1,7 @@
+message="The name of Barcelona's best forward is Lionel Andres Messi and he is from Argentina"
+count={}
+for character in message:
+    count.setdefault(character,0)
+    count[character]=count[character]+1
+
+print(count)
